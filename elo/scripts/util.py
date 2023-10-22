6ccea0dd180d0ff1f47dd9f2e644be59c03a2b57
stevenjang1729 <0331steven@gmail.com>
2023-10-22 02:07:56 -0700

    elods

---
+++ elo/scripts/util.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+def getFile(path):
+    fd = os.open(path, os.O_RDONLY)
+    data = os.read(fd, os.path.getsize(path))
+    data = json.loads(data)
+    os.close(fd)
+    return data
+
+def writeFile(path, data):
+    data_bytes = json.dumps(data).encode('utf-8')
+    fd = os.open(path, os.O_WRONLY | os.O_CREAT)
+    os.write(fd, data_bytes)
+    os.close(fd)
