ec45d2aea34f397c5f1b95d8d95cbdfddea8a38c
Soung Bae Kim <sbkim@berkeley.edu>
2023-10-23 04:06:09 -0700

    clean up

--- parser/main.go
+++ parser/main.go
@@ -6,9 +6,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"sync"
-	"sync/atomic"
-	"time"
 
 	"github.com/minio/simdjson-go"
 )
@@ -21,61 +18,10 @@ const (
 
 func main() {
 	run()
-	// run_old()
 }
 
-func run_old() {
-	games := parse[map[string]any](GAMES_FILE)
-	fmt.Printf("length -- %d", len(games))
-
-	var wg sync.WaitGroup
-	sema := make(chan struct{}, 30)
-
-	start := time.Now()
-
-	var count atomic.Int32
-
-	for i, g := range games {
-		wg.Add(1)
-		sema <- struct{}{}
-		go func(game map[string]any) {
-			defer wg.Done()
-			defer func() { <-sema }()
-
-			gameID := game["id"].(string)
-			// defer func() {
-			// 	if r := recover(); r != nil {
-			// 		count.Add(1)
-			// 		fmt.Printf("errors: %d -- %s\n", count.Load(), gameID)
-			// 	}
-			// }()
-
-			// os.Remove(getGamePath(gameID))
-			// return
-
-			start := time.Now()
-
-			data := getGame(gameID)
-			if len(data) == 0 {
-				fmt.Println("no file found")
-				return
-			}
-
-			_, err := analyze(data)
-			if err != nil {
-				panic(fmt.Sprintf("%s -- %s\n", gameID, err.Error()))
-			}
-
-			fmt.Printf("game analyzed -- %s\n", time.Since(start))
-		}(g)
-
-		if i%100 == 0 {
-			fmt.Printf("checkpoint -- %d -- %s\n", i, time.Since(start))
-		}
-	}
-
-	wg.Wait()
-	fmt.Printf("errors: %d -- %s\n", count.Load(), time.Since(start))
+func getGamePath(gameID string) string {
+	return fmt.Sprintf("%s/%s.json", GAMES_DIRECTIORY, gameID)
 }
 
 func parse[T any](path string) []T {
@@ -113,79 +59,7 @@ func write(path string, reader io.Reader) {
 	}
 }
 
-func getGamePath(gameID string) string {
-	return fmt.Sprintf("%s/%s.json", GAMES_DIRECTIORY, gameID)
-}
-
-func getGame(gameID string) Events {
-	return parse[Event](fmt.Sprintf("%s/%s.json", GAMES_DIRECTIORY, gameID))
-}
-
-func analyze(events Events) (*Game, error) {
-	game := Game{
-		Start: events[0].EventTime(),
-		End:   events[0].EventTime(),
-	}
-
-	for i, event := range events {
-		t, ok := event["eventType"].(string)
-		if !ok {
-			return nil, fmt.Errorf("unable to parse event type: %d", i)
-		}
-
-		eventType := EventType(t)
-		switch eventType {
-		case BULDING_DESTROYED:
-		case CHAMPION_KILL:
-		case CHAMPION_KILL_SPECIAL:
-		case CHAMPION_LEVEL_UP:
-		case EPIC_MONSTER_KILL:
-		case GAME_INFO:
-		case GAME_END:
-		case ITEM_DESTROYED:
-		case ITEM_PURCHASED:
-		case ITEM_SOLD:
-		case SKILL_LEVEL_UP:
-		case STATS_UPDATE:
-		case SUMMONER_SPELL_USED:
-		case TURRET_PLATE_DESTROYED:
-		case WARD_KILLED:
-		case WARD_PLACED:
-		case QUEUED_EPIC_MONSTER_INFO:
-		case QUEUED_DRAGON_INFO:
-		case EPIC_MONSTER_SPAWN:
-		case TURRET_PLATE_GOLD_EARNED:
-		case ITEM_UNDO:
-		case OBJECTIVE_BOUNTY_PRESTART:
-		case OBJECTIVE_BOUNTY_FINISH:
-		case SURRENDER_VOTE_START:
-		case SURRENDER_FAILED_VOTES:
-		case SURRENDER_VOTE:
-		case SURRENDER_AGREED:
-		case CHAMPION_REVIVED:
-		case CHAMPION_TRANSFORMED:
-		case UNANIMOUS_SURRENDER_VOTE_START:
-		case CHAMP_SELECT:
-		default:
-			panic(fmt.Sprintf("unknown event type: %s", t))
-		}
-
-		// fmt.Printf("event type %s\n", eventType)
-
-		eventTime := event.EventTime()
-
-		if eventTime.Before(game.Start) {
-			game.Start = eventTime
-		}
-		if eventTime.After(game.End) {
-			game.End = eventTime
-		}
-	}
-
-	return &game, nil
-}
-
-func simdAnal(events simdjson.ParsedJson) (*Game, error) {
+func analyze(events simdjson.ParsedJson) (*Game, error) {
 	game := Game{}
 
 	eventsIter := events.Iter()
--- parser/simd.go
+++ parser/simd.go
@@ -23,23 +23,6 @@ func run() {
 
 	var count atomic.Int32
 
-	// data := make(chan *Game, WORKERS)
-	// go func() {
-	// 	toRet := []map[string]any{}
-	// 	for game := range data {
-	// 		currentGame := games[i]
-	// 		game["start_time"] = gameTimes.Start
-	// 		game["end_time"] = gameTimes.End
-	// 	}
-
-	// 	b, err := json.Marshal(games)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	write(OUTPUT_PATH, bytes.NewBuffer(b))
-	// }()
-
 	start := time.Now()
 	for i, g := range games {
 		wg.Add(1)
@@ -50,7 +33,7 @@ func run() {
 
 			gameID := game["id"].(string)
 
-			parsed, err := simdParse(fmt.Sprintf("%s/%s.json", GAMES_DIRECTIORY, gameID))
+			parsed, err := simdParse(getGamePath(gameID))
 			if parsed == nil {
 				fmt.Printf("parsed is null -- %s\n", err)
 				return
@@ -62,7 +45,7 @@ func run() {
 				return
 			}
 
-			gameTimes, err := simdAnal(*parsed)
+			gameTimes, err := analyze(*parsed)
 			if err != nil {
 				fmt.Printf("analysis error -- %s\n", err)
 			}
@@ -87,33 +70,6 @@ func run() {
 	write(OUTPUT_PATH, bytes.NewBuffer(b))
 	fmt.Printf("errors: %d -- %s\n", count.Load(), time.Since(start))
 	fmt.Println("------------")
-}
-
-func strategicGetGame(gameID string, unmarshalFn func(any, bytes.Buffer) error) Events {
-	return strategicParse[Event](fmt.Sprintf("%s/%s.json", GAMES_DIRECTIORY, gameID), unmarshalFn)
-}
-
-// Parse functions
-func strategicParse[T any](path string, unmarshalFn func(any, bytes.Buffer) error) []T {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil
-	}
-	defer file.Close()
-
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, file)
-	if err != nil {
-		panic(err)
-	}
-
-	var data []T
-	err = unmarshalFn(&data, buf)
-	if err != nil {
-		fmt.Printf("error unmarshaling -- %s", err)
-	}
-
-	return data
 }
 
 func simdParse(path string) (*simd.ParsedJson, error) {
